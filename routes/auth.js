7e912b06c8b1f3039416b3ba625b3b3cf251e031
Ahmed <a.hennawy97@hotmail.com>
2024-08-23 16:16:55 +0300

    "commit up to date code"

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,30 @@
+const Router = require("express").Router;
+const router = new Router();
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../config");
+const User = require("../models/user");
+const ExpressError = require("../expressError");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
+    }
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +32,16 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username } = await User.register(req.body);
+    let token = jwt.sign({ username }, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({ token });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
+const Router = require("express").Router;
+const router = new Router();
+const { ensureCorrectUser, ensureLoggedIn } = require("../middleware/auth");
+const User = require("../models/user");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.all();
+    return res.json({ users });
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** GET /:username - get detail of users.
  *
@@ -11,6 +23,15 @@
  *
  **/
 
+router.get("/:username", async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const user = await User.get(username);
+    return res.json({ user });
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -22,7 +43,15 @@
  *
  **/
 
-
+router.get("/:username/to", async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const messages = User.messagesTo(username);
+    return res.json({ messages });
+  } catch (e) {
+    return next(e);
+  }
+});
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +61,15 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from", async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const messages = User.messagesFrom(username);
+    return res.json({ messages });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
